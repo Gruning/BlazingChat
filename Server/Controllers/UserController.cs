3e76baa5424d8f320bc9646e26433e9e644f07c2
AlbertoGruning <alberto.gruning.zen@gmail.com>
2021-01-09 19:40:27 -0300

    userController

---
+++ Server/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using BlazingChat.Shared;
+using BlazingChat.Server.Models;
+
+namespace BlazingChat.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public List<User> Get()
+        {
+
+        }
+    }
+}
