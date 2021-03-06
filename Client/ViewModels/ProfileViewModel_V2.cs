4fcb34eda660638d5b08f7adaa3836f8184c7ad0
AlbertoGruning <alberto.gruning.zen@gmail.com>
2021-03-06 16:09:22 -0300

    cap8-DependencyInyectionInMvvm

--- Client/ViewModels/ProfileViewModel_V2.cs
+++ Client/ViewModels/ProfileViewModel_V2.cs
@@ -1,8 +1,9 @@
 using BlazingChat.Shared.Models;
 
 namespace BlazingChat.ViewModels{
-    public class ProfileViewModel:IProfileViewModel
+    public class ProfileViewModel_V2:IProfileViewModel
     {
+       
         public long UserId {get;set;}
         public string FirstName {get;set;}
         public string LastName {get;set;}
@@ -12,22 +13,23 @@ namespace BlazingChat.ViewModels{
     public void updateProfile(){
     //    User user = _profileViewModel;
     //   await HttpClient.PutAsJsonAsync("user/updateprofile/10",user);
-        this.Message="Profile updated successfully";
+        this.Message=this.FirstName + "'s Profile updated successfully --faster";
     }
     public void getProfile(){
         //User user = await HttpClient.GetFromJsonAsync<User>("user/getprofile/10");
         //_profileViewModel = user;
-        this.Message="Profile loaded successfully";
+       this.FirstName="NewDriver"; 
+        this.Message=this.FirstName + "'s Profile loaded successfully --faster";
     }
-        public static implicit operator ProfileViewModel(User user){
-            return new ProfileViewModel{
+        public static implicit operator  ProfileViewModel_V2(User user){
+            return new ProfileViewModel_V2{
                 FirstName = user.FirstName,
                 LastName = user.LastName,
                 EmailAddress = user.EmailAddress,
                 UserId = user.UserId,
             };
         }
-        public static implicit operator User(ProfileViewModel profileViewModel){
+        public static implicit operator User(ProfileViewModel_V2 profileViewModel){
             return new User{
                 FirstName = profileViewModel.FirstName,
                 LastName = profileViewModel.LastName,
